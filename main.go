1e75002f4e23ce882235feb9f5d2444f615cadad
fvdime <73942727+fvdime@users.noreply.github.com>
2023-11-16 18:11:19 +0300

    add chi into the project

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/go-chi/chi/v5"
+)
+
+func main(){
+	server := &http.Server{
+		Addr: ":3003",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Failed to listen to server", err)
+	}
+}
+
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("HELLO  WORLD!!!"))
+}
