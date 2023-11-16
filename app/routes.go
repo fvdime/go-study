a0f350fc9af6b0cc7cceec51ce0d75bc2aa66371
fvdime <73942727+fvdime@users.noreply.github.com>
2023-11-16 21:32:58 +0300

    add create handler

--- app/routes.go
+++ app/routes.go
@@ -2,13 +2,15 @@ package app
 
 import (
 	"net/http"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 
 	"github.com/fvdime/go-study/handler"
+	"github.com/fvdime/go-study/repository/order"
 )
 
-func loadRoutes() *chi.Mux {
+func (a *App) loadRoutes() {
 	router := chi.NewRouter()
 
 	router.Use(middleware.Logger)
@@ -17,13 +19,17 @@ func loadRoutes() *chi.Mux {
 		w.WriteHeader(http.StatusOK)
 	})
 
-	router.Route("/orders", loadOrderRoutes)
+	router.Route("/orders", a.loadOrderRoutes)
 
-	return router
+	a.router = router
 }
 
-func loadOrderRoutes(router chi.Router){
-	orderHandler := &handler.Order{}
+func (a *App) loadOrderRoutes(router chi.Router){
+	orderHandler := &handler.Order{
+		Repository: &order.RedisRepository{
+			Client: a.rdb,
+		},
+	}
 
 	router.Post("/", orderHandler.Create)
 	router.Get("/", orderHandler.List)
--- handler/order.go
+++ handler/order.go
@@ -3,28 +3,73 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"encoding/json"
+	"math/rand"
+	"time"
+
+	"github.com/google/uuid"
+
+	"github.com/fvdime/go-study/model"
+	"github.com/fvdime/go-study/repository/order"
 )
 
 type Order struct {
-	router http.Handler
+	Repository *order.RedisRepository
 }
 
-func (o *Order) Create(w http.ResponseWriter, r *http.Request){
-	fmt.Println("create an order")
+// not working lol
+func (h *Order) Create(w http.ResponseWriter, r *http.Request){
+	var body struct{
+		UserId uuid.UUID `json:"user_id"`
+		Items []model.Item `json:"items"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	now := time.Now().UTC()
+
+	// not good 4 production
+	order := model.Order{
+		OrderId: rand.Uint64(),
+		UserId: body.UserId,
+		Items: body.Items,
+		CreatedAt: &now,
+	}
+
+	// inserting into the repository
+	err := h.Repository.Insert(r.Context(), order)
+	if err != nil {
+		fmt.Println("failed to insert:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	res, err := json.Marshal(order)
+	if err != nil {
+		fmt.Println("failed to encode: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(res)
+	w.WriteHeader(http.StatusCreated)
 }
 
-func (o *Order) List(w http.ResponseWriter, r *http.Request){
+func (h *Order) List(w http.ResponseWriter, r *http.Request){
 	fmt.Println("list orders")
 }
 
-func (o *Order) GetById(w http.ResponseWriter, r *http.Request){
+func (h *Order) GetById(w http.ResponseWriter, r *http.Request){
 	fmt.Println("get an order by id")
 }
 
-func (o *Order) UpdateById(w http.ResponseWriter, r *http.Request){
+func (h *Order) UpdateById(w http.ResponseWriter, r *http.Request){
 	fmt.Println("update an order by id")
 }
 
-func (o *Order) DeleteById(w http.ResponseWriter, r *http.Request){
+func (h *Order) DeleteById(w http.ResponseWriter, r *http.Request){
 	fmt.Println("delete an order by id")
 }
